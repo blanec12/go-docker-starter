79fabcaf58557109e0b652deaa6cad229cc375b6
Blane Cummings <bcummings@portacool.com>
2025-05-23 11:38:47 -0500

    fix syntax error in main.go

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -6,9 +6,9 @@ import (
 )
 
 func main() {
-  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request)) {
+  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
     fmt.Println(w, "Go app running with hot reload")
-  }
+  })
 
   fmt.Println("Listening on :8080")
   http.ListenAndServe(":8080", nil)
